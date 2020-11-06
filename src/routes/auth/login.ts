883c319f3617e69903d7a1e273f92404267361de
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-07 00:33:37 +0900

    feat: auth router

---
+++ src/routes/auth/login.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+
+const router = Router();
+
+router.post('/', expressAsyncHandler((req, res) => {
+
+}))
+
+export default router;
