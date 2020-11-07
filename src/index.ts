c9a46feb5dd4ff19e816e5fe4bbfb7f92e97b0be
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-07 11:06:32 +0900

    fix: add options in mongoose connect
    useNewUrlParser
    useUnifiedTopology

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import mongoose from 'mongoose';
 import app from './app';
 import { mongodb as mongoConf } from '../config.json';
 
-mongoose.connect(`mongodb://`
-                + `${mongoConf.name}:${encodeURIComponent(mongoConf.password)}`
-                + `@${mongoConf.host}:${mongoConf.port}`
-                + `/${mongoConf.db}?authSource=${mongoConf.authSource}`);
+mongoose.connect(
+  'mongodb://'
+    + `${mongoConf.name}:${encodeURIComponent(mongoConf.password)}`
+    + `@${mongoConf.host}:${mongoConf.port}`
+    + `/${mongoConf.db}?authSource=${mongoConf.authSource}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 app.listen(8080);
