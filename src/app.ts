d9e2398f7ad97f41300a3432d3070d429475db0b
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-07 11:24:05 +0900

    refactor: lint

--- src/app.ts
+++ src/app.ts
@@ -19,10 +19,11 @@ app.use(express.json());
 
 app.use('/', router);
 
-app.use((err: IError, req: Request, res: Response, next: NextFunction) => {
+app.use((err: IError, req: Request, res: Response) => {
   if (err.type === 'entity.parse.failed') {
     res.send({ success: false, code: 400, msg: 'Invalid json type' });
   } else {
+    // eslint-disable-next-line no-console
     console.error(err.stack);
     res.status(err.status || 500).json({
       message: err.data,
