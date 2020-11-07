3bd5dfa69d5c11ad701ac38cdc54347d8292624b
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-07 09:22:05 +0900

    feat: Identity interface

---
+++ src/types/dimigo.ts
@@ -0,0 +1,12 @@
+interface IIdentity {
+  user_type: 'S' | 'T' | 'D' | 'G' | 'P';
+  email: string;
+  name: string;
+  idx: number;
+  id: string;
+  klass: number;
+  photo: string;
+  grade: number;
+  number: number;
+  serial: number;
+};
