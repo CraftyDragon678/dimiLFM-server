f907e8d6054ec79a7943cdd0006e3dcb055ff328
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-09 00:40:36 +0900

    refactor: dimigo login type

--- src/utils/dimigoinLogin.ts
+++ src/utils/dimigoinLogin.ts
@@ -2,16 +2,16 @@ import fetch from 'node-fetch';
 import jwt from 'jsonwebtoken';
 import { IIdentity } from '../types/dimigo';
 
-const loginAsDimigoIn = async (id: string, password: string) => {
+const loginAsDimigoIn = async (id: string, password: string): Promise<IIdentity | undefined> => {
   const { token }: { token: string } = await (await fetch('https://dev-api.dimigo.in/auth/', {
     method: 'POST',
-    body: JSON.stringify({id, password}),
+    body: JSON.stringify({ id, password }),
     headers: {
       'content-type': 'application/json',
     },
   })).json();
-  const payload = jwt.decode(token) as { identity: IIdentity[] };
-  return payload.identity[0];
-}
+  const payload = jwt.decode(token) as { identity: IIdentity[] } | null;
+  return payload?.identity[0];
+};
 
 export default loginAsDimigoIn;
