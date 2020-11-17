3b99989360cab850e67eea58feb1f3d18a678fb7
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-17 19:28:15 +0900

    fix: check jwt error

--- src/middleware/needAuth.ts
+++ src/middleware/needAuth.ts
@@ -1,10 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 import { jwtSecret } from '../../config.json';
 
 const needAuth = (req: Request, res: Response, next: NextFunction): void => {
   if (req.cookies.token) {
-    req.auth = jwt.verify(req.cookies.token, jwtSecret) as never;
+    try {
+      req.auth = jwt.verify(req.cookies.token, jwtSecret) as never;
+    } catch (e) {
+      if (e instanceof JsonWebTokenError || e instanceof TokenExpiredError ) {
+        res.status(401).send();
+      }
+    }
     next();
   } else {
     res.status(401).send();
