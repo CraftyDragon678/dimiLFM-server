db2787bacf0bf5e338710b3fdcd6e83cb6c32653
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-22 10:41:16 +0900

    feat: redis async

--- src/redis.ts
+++ src/redis.ts
@@ -2,4 +2,26 @@ import redis from 'redis';
 
 const redisClient = redis.createClient();
 
+interface Hash {
+  [key: string]: string | number;
+}
+
+export const hmset = (key: string, obj: Hash): Promise<'OK'> => (
+  new Promise((resolve, reject) => {
+    redisClient.hmset(key, obj, (err, reply) => {
+      if (err) reject(err);
+      else resolve(reply);
+    });
+  })
+);
+
+export const hgetall = (key: string): Promise<Hash> => (
+  new Promise((resolve, reject) => {
+    redisClient.hgetall(key, (err, reply) => {
+      if (err) reject(err);
+      else resolve(reply);
+    });
+  })
+)
+
 export default redisClient;
