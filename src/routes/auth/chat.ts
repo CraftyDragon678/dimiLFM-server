4837e641e776d51073f0771e75e923b034e9bbd2
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-22 10:07:39 +0900

    feat: make chat token

--- src/routes/auth/chat.ts
+++ src/routes/auth/chat.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express';
 import expressAsyncHandler from 'express-async-handler';
+import crypto from 'crypto';
+import redisClient from '../../redis';
 
 const router = Router();
 
 router.get('/', expressAsyncHandler(async (req, res) => {
-  console.log(req.auth);
-  res.send();
+  const now = new Date();
+  now.setSeconds(now.getSeconds() + 30);
+  now.getTime();
+
+  const token = crypto.randomBytes(64).toString('base64');
+
+  redisClient.hmset(`socket/${req.auth.oid}`, 'token', token, 'expire', now.getTime());
+  res.json({ token, exp: now.getTime() });
 }));
 
 export default router;
