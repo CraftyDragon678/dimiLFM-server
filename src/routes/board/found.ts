929103e69af9fec6a65395db2c712c1afba58015
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-29 01:29:09 +0900

    feat: check radioIndex is valid

--- src/routes/board/found.ts
+++ src/routes/board/found.ts
@@ -6,9 +6,9 @@ const router = Router();
 
 router.post('/', expressAsyncHandler(async (req, res) => {
   const { foundDate: [from, to], radioIndex, ...data }: IFoundPayload = req.body;
-  if (!(radioIndex >= 0 && radioIndex <= 2)) {
+  if (!(radioIndex >= 0 && radioIndex <= 2 && Number.isInteger(radioIndex))) {
     return res.status(400).json({
-      message: 'radioIndex must be greater than or 0 to zero and less than or equal to 2',
+      message: 'radioIndex must be integer and greater than or 0 to zero and less than or equal to 2',
     });
   }
 
