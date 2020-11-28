682b64364d60bab0bf040bf3ac72aab7892023da
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-28 23:31:30 +0900

    feat: handle board endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import auth from './auth';
+import board from './board';
 import socket from './socket';
 
 const router = Router();
 
 router.use('/auth', auth);
+router.use('/board', board);
 router.use('/socket', socket);
 
 export default router;
