40a3dd58688a52625ea579c760509165903c4745
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-29 02:43:04 +0900

    feat: check market payload validation

--- src/routes/board/market.ts
+++ src/routes/board/market.ts
@@ -7,10 +7,22 @@ const router = Router();
 router.post('/', expressAsyncHandler(async (req, res) => {
   const { beforePrice, afterPrice, ...data }: IMarketPayload = req.body;
 
+  if (!Number.isSafeInteger(beforePrice) || beforePrice < 0) {
+    return res.status(400).json({
+      message: 'beforePrice must be safe integer and positive',
+    });
+  }
+
+  if (!Number.isSafeInteger(afterPrice) || afterPrice < 0) {
+    return res.status(400).json({
+      message: 'beforePrice must be safe integer and positive',
+    });
+  }
+
   const result = await Markets.create({
     ...data,
-    beforePrice: beforePrice.toString(),
-    afterPrice: afterPrice.toString(),
+    beforePrice,
+    afterPrice,
     user: req.auth.oid,
   });
   return res.json({ _id: result._id });
