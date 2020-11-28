5c9ea708d73cd24ebeb8dab7a0b00a67ab0fbb35
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-29 01:26:37 +0900

    feat: found radioIndex

--- src/routes/board/found.ts
+++ src/routes/board/found.ts
@@ -6,13 +6,20 @@ const router = Router();
 
 router.post('/', expressAsyncHandler(async (req, res) => {
   const { foundDate: [from, to], radioIndex, ...data }: IFoundPayload = req.body;
-  await Founds.create({
+  if (!(radioIndex >= 0 && radioIndex <= 2)) {
+    return res.status(400).json({
+      message: 'radioIndex must be greater than or 0 to zero and less than or equal to 2',
+    });
+  }
+
+  const result = await Founds.create({
     ...data,
     from,
     to,
+    radioIndex,
     user: req.auth.oid,
   });
-  res.status(204).send();
+  res.json({ _id: result._id });
 }));
 
 export default router;
