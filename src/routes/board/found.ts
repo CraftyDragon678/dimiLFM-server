d9891ac1f07546e53f94eea16588ba8acf3a0528
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-28 23:45:39 +0900

    feat: founds create

--- src/routes/board/found.ts
+++ src/routes/board/found.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import expressAsyncHandler from 'express-async-handler';
-import { Founds } from '../../models/boardModel';
+import { Founds, IFoundPayload } from '../../models/boardModel';
 
 const router = Router();
 
 router.post('/', expressAsyncHandler(async (req, res) => {
-  const data = req.body;
-  Founds.create(data);
+  const { foundDate: [from, to], radioIndex, ...data }: IFoundPayload = req.body;
+  await Founds.create({
+    ...data,
+    from,
+    to,
+    user: req.auth.oid,
+  });
   res.status(204).send();
 }));
 
