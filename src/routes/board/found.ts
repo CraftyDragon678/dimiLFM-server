86a502950271ec36149001827c95aa2e289a57d1
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-29 21:09:07 +0900

    feat: found search

--- src/routes/board/found.ts
+++ src/routes/board/found.ts
@@ -22,4 +22,52 @@ router.post('/', expressAsyncHandler(async (req, res) => {
   return res.json({ _id: result._id });
 }));
 
+interface SearchPayload {
+  option: {
+    done: boolean;
+    notdone: boolean;
+    old: boolean;
+    my: boolean;
+  };
+  tags: string[];
+  dates: Date[];
+  location: string[];
+}
+
+router.post('/search', expressAsyncHandler(async (req, res) => {
+  const {
+    option: {
+      done, notdone, old, my,
+    },
+    tags,
+    dates,
+    location,
+  }: SearchPayload = req.body;
+
+  const parsedDates = dates.map((e) => new Date(e));
+
+  const result = await Founds.find({
+    $and: [
+      {
+        $or: [
+          { done },
+          { done: !notdone },
+        ],
+      },
+      {
+        tag: { $in: tags },
+        ...location.length && { foundLocation: { $in: location } },
+        ...my && { user: req.auth.oid },
+        $or: [
+          { createdAt: { $gte: parsedDates[0], $lte: parsedDates[1] } },
+          { from: { $lte: parsedDates[1] } },
+          { to: { $gte: parsedDates[0] } },
+        ],
+      },
+    ],
+  }).sort({ createdAt: old ? -1 : 1 });
+
+  return res.json({ data: result });
+}));
+
 export default router;
