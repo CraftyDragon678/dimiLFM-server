7c42e09147e1b3dbed48f569447cfef8727b3d81
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-01 17:39:16 +0900

    feat: check message is valid

--- src/socket/handlers/send.ts
+++ src/socket/handlers/send.ts
@@ -4,10 +4,11 @@ import * as redis from '../../redis';
 export default {
   event: 'send',
   listener(msg: { type: string, message: string }) {
+    if (!msg.message.trim()) return;
     (async () => {
       const client = await redis.hgetall(`client/${this.id}`);
       this.broadcast.emit('message', `${client.name}: ${msg.message}`);
-      this.emit('send', 'ok');
+      this.emit('send', true);
     })();
   },
 } as SocketEventHandler;
