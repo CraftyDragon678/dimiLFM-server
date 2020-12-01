2033f5f9485e046b05911d3a9031370318d11511
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-01 16:11:19 +0900

    feat: market search

--- src/routes/board/market.ts
+++ src/routes/board/market.ts
@@ -28,4 +28,60 @@ router.post('/', expressAsyncHandler(async (req, res) => {
   return res.json({ _id: result._id });
 }));
 
+interface SearchPayload {
+  option: {
+    done: boolean;
+    notdone: boolean;
+    old: boolean;
+    my: boolean;
+  };
+  tags: string[];
+  dates: Date[];
+  location: string[];
+}
+
+router.post('/search', expressAsyncHandler(async (req, res) => {
+  const {
+    option: {
+      done, notdone, old, my,
+    },
+    tags,
+    dates,
+    location,
+  }: SearchPayload = req.body;
+
+  const parsedDates = dates.map((e) => new Date(e));
+
+  const result = await Markets.find({
+    $and: [
+      {
+        $or: [
+          { done },
+          { done: !notdone },
+        ],
+      },
+      {
+        tag: { $in: tags },
+        ...location.length && { foundLocation: { $in: location } },
+        ...my && { user: req.auth.oid },
+        $or: [
+          { createdAt: { $gte: parsedDates[0], $lte: parsedDates[1] } },
+          { from: { $lte: parsedDates[1] } },
+          { to: { $gte: parsedDates[0] } },
+        ],
+      },
+    ],
+  }).sort({ createdAt: old ? 1 : -1 })
+    .select('title user done createdAt content')
+    .populate('user', '-_id serial name')
+    .lean();
+
+  return res.json({
+    data: result.map(({ content, ...e }) => ({
+      ...e,
+      image: content.match(/src="(.*?)"/)?.[1],
+    })),
+  });
+}));
+
 export default router;
