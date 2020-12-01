153a40f546d57d5e4692c5237645731e9b7d38b3
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-01 22:48:58 +0900

    feat: use query builder instead of aggregate

--- src/routes/chat/list.ts
+++ src/routes/chat/list.ts
@@ -5,24 +5,15 @@ import { Chats } from '../../models/chatModel';
 const router = Router();
 
 router.get('/', expressAsyncHandler(async (req, res) => {
-  const result = await Chats.aggregate([
-    {
-      $match: {
-        $or: [
-          { from: req.auth.oid },
-          { to: req.auth.oid },
-        ],
-      },
-    },
-    {
-      $project: {
-        messages: { $slice: ['$messages', -1] },
-        from: 1,
-        to: 1,
-      },
-    },
-  ]);
-  return res.json(result.map((e) => ({ ...e, lastMessage: e.messages[0] })));
+  const result = await Chats.find().or([
+    { from: req.auth.oid },
+    { to: req.auth.oid },
+  ]).select('messages from to')
+    .where('messages')
+    .slice(-1)
+    .lean();
+
+  return res.json(result.map(({ messages, ...e }) => ({ ...e, lastMessage: messages[0] })));
 }));
 
 export default router;
