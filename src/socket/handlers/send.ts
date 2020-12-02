28680e0fd1422c9e46bcbae8beb307cf287db6de
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-02 12:07:38 +0900

    feat: send socket event

--- src/socket/handlers/send.ts
+++ src/socket/handlers/send.ts
@@ -1,13 +1,47 @@
 import { SocketEventHandler } from '.';
-import * as redis from '../../redis';
+import * as clients from '../clients';
+import { Chats } from '../../models/chatModel';
 
 export default {
   event: 'send',
-  listener(msg: { type: string, message: string }) {
+  listener(msg: { channel: string, type: string, message: string }) {
     if (!msg.message.trim()) return;
     (async () => {
-      const client = await redis.hgetall(`client/${this.id}`);
-      this.broadcast.emit('message', `${client.name}: ${msg.message}`, new Date());
+      const channel = await Chats.findById(msg.channel);
+      if (!channel) {
+        this.emit('send', false);
+        return;
+      }
+      const isFrom = channel.from === clients.getOid(this);
+
+      await Chats.findByIdAndUpdate(msg.channel, {
+        $push: {
+          messages: {
+            from: isFrom,
+            message: msg.message,
+            type: msg.type,
+          },
+        },
+      });
+
+      clients.getAll((isFrom ? channel.to : channel.from) as number).forEach((socket) => {
+        socket.emit('message', {
+          type: msg.type,
+          channel: msg.channel,
+          message: msg.message,
+          date: new Date(),
+        });
+      });
+      clients.getAll((isFrom ? channel.from : channel.to) as number).forEach((socket) => {
+        if (socket === this) return;
+        socket.emit('message', {
+          mine: true,
+          type: msg.type,
+          channel: msg.channel,
+          message: msg.message,
+          date: new Date(),
+        });
+      });
       this.emit('send', true);
     })();
   },
